90a56304c47b0ad2500359a9dfc99b90646fb3ab
Ianek0 <16bitstreams@gmail.com>
2021-06-14 21:10:20 +0500

    upd

---
+++ FileEncoder2.0/FileEncoder2.0/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace File_Encoder
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the command: ");
+            string[] currentCommand = Console.ReadLine().Split(' ', '-');
+            var smthng = currentCommand.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+            switch (smthng[1])
+            {
+                case"encode":
+                    Encoding.ExecuteCommand(smthng);
+                    break;
+                case"decode":
+                    Decoding.ExecuteCommand(smthng);
+                    break;
+            }
+        }
+    }
+    class Encoding
+    {
+        static Source GetSource(string[] cmnd, string srs)
+        {
+            return srs == cmnd[3] ? Source.buffer : Source.source;
+        }
+        public static void ExecuteCommand(string[] cmnd)
+        {
+            string file = File.ReadAllText($"{cmnd[5]}");
+            var encoded = System.Text.Encoding.UTF8.GetBytes(file);
+            if (GetSource(cmnd, cmnd[3]) == Source.source)
+            {
+                File.WriteAllText($"{cmnd[5]}.txt", System.Convert.ToBase64String(encoded));
+            }
+            else Clipboard.SetText(System.Convert.ToBase64String(encoded));
+        }
+    }
+
+    class Decoding
+    {
+        static Source GetSource(string[] cmnd, string srs)
+        {
+            return srs == cmnd[3] ? Source.buffer : Source.source;
+        }
+        public static void ExecuteCommand(string[] cmnd)
+        {
+            string file = File.ReadAllText($"{cmnd[5]}");
+            var decoded = System.Convert.FromBase64String(file);
+            if (GetSource(cmnd, cmnd[3]) == Source.source)
+            {
+                File.WriteAllText($"{cmnd[5]}.txt", System.Text.Encoding.UTF8.GetString(decoded));
+            }
+            else Clipboard.SetText(System.Text.Encoding.UTF8.GetString(decoded));
+        }
+    }
+    enum Commands { encode, decode }
+    enum Source { buffer, source }
+}
