fb6d88f03bccb20a15a0c1e4bc07731935a9850c
Ianek0 <16bitstreams@gmail.com>
2021-06-25 21:08:25 +0500

    upd xmlConv

--- XmlConverter/Program.cs
+++ XmlConverter/Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 class Solution
 {
-    public static List<Member> Members = new List<Member>();
+    public static readonly List<Member> Members = new List<Member>();
     static void Main()
     {
         string[] FileInformation = File.ReadAllLines("membs.xml");
@@ -20,66 +20,36 @@ class Solution
             newInformation.Add($"{new string(' ', 2)}<member name=\"{Members[i].Name}\">");
             foreach(var rolesprojs in Members[i].RoleName)
             {
-                newInformation.Add($"{new string(' ', 4)}member role=\"{rolesprojs.RoleName}\" project=\"{rolesprojs.ProjectName}\"");
+                newInformation.Add($"{new string(' ', 4)}<member role=\"{rolesprojs.RoleName}\" project=\"{rolesprojs.ProjectName}\"/>");
             }
             newInformation.Add($"{new string(' ', 2)}<\\member>");
         }
         newInformation.Add("<\\members>");
         File.WriteAllLines("updMembs.xml", newInformation);
     }
-    static void GetInformation(string[] info)
+    public static void GetInformation(string[] info)
     {
         string currentProject = "";
-        for(int i = 0; i < info.Length; i++)
+        for (int i = 0; i < info.Length; i++)
         {
-            string[] str = info[i].Split(new char[] { '<', '>', '=', '"', ' '}, StringSplitOptions.RemoveEmptyEntries);
-            if (IsProject(str) == true)
+            string[] str = info[i].Split(new char[] { '<', '>', '=', '"', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (Member.IsProject(str) == true)
                 currentProject = str[2];
-            if(IsMember(str) == true)
+            if (Member.IsMember(str) == true)
             {
-                if (ContainsMember(str[4]))
+                if (Member.ContainsMember(str[4]))
                 {
-                    FillMembersRoles(str[4], str[2], currentProject);
+                    Role.FillMembersRoles(str[4], str[2], currentProject);
                 }
                 else
                 {
                     Members.Add(new Member(new List<Role>(), str[4]));
-                    FillMembersRoles(str[4], str[2], currentProject);
+                    Role.FillMembersRoles(str[4], str[2], currentProject);
                 }
             }
         }
     }
-    static bool ContainsMember(string mName)
-    {
-        foreach(var member in Members)
-        {
-            if (member.Name == mName)
-                return true;
-        }
-        return false;
-    }
-    static void FillMembersRoles(string mName, string roleName, string projectName)
-    {
-        foreach(var member in Members)
-        {
-            if(member.Name == mName)
-            {
-                member.RoleName.Add(new Role(roleName, projectName));
-            }
-        }
-    }
-    static bool IsMember(string[] str)
-    {
-        if (str[0] == "member")
-            return true;
-        return false;
-    }
-    static bool IsProject(string[] str)
-    {
-        if (str[0] == "project")
-            return true;
-        return false;
-    }
+
 }
 class Member
 {
@@ -90,6 +60,19 @@ class Member
         RoleName = role;
         Name = name;
     }
+    
+    public static bool ContainsMember(string mName)
+    {
+        foreach (var member in Solution.Members)
+        {
+            if (member.Name == mName)
+                return true;
+        }
+        return false;
+    }
+    
+    public static bool IsMember(string[] str) => str[0] == "member";
+    public static bool IsProject(string[] str) => str[0] == "project";
 }
 class Role
 {
@@ -100,5 +83,15 @@ class Role
     {
         RoleName = rName;
         ProjectName = pName;
+    }
+    public static void FillMembersRoles(string mName, string roleName, string projectName)
+    {
+        foreach (var member in Solution.Members)
+        {
+            if (member.Name == mName)
+            {
+                member.RoleName.Add(new Role(roleName, projectName));
+            }
+        }
     }
 }
